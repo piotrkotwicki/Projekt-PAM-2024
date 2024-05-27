e41901198b1e8b1464ec9b0da7992d3624925764
cotvi <piotrkotwicki74@gmail.com>
2024-05-27 10:08:04 +0200

    Początek implementacji mapy

--- MovieApp/app/src/main/java/com/project/movieapp/ActivityOne.kt
+++ MovieApp/app/src/main/java/com/project/movieapp/ActivityOne.kt
@@ -5,16 +5,27 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class ActivityOne : AppCompatActivity() {
+class ActivityOne : AppCompatActivity(), OnMapReadyCallback {
+    private var mGoogleMap:GoogleMap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_one)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentByID(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         val buttonReturnToMain = findViewById<Button>(R.id.buttonReturnToMain)
         buttonReturnToMain.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             overridePendingTransition(0, 0)
+
+
         }
+
+        }
+    override fun onMapReady(googleMap: GoogleMap) {
+        mGoogleMap = googleMap
     }
 }
--- MovieApp/app/src/main/java/com/project/movieapp/MainActivity.kt
+++ MovieApp/app/src/main/java/com/project/movieapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.project.movieapp
+ package com.project.movieapp
 
 import android.Manifest
 import android.app.NotificationChannel
